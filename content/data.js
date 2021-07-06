73be8ca4a0e9e5c3741edb1d93a9b7726810d5d6
Bogdan5 <bogdan5x@gmail.com>
2021-07-06 19:44:24 -0400

    Logic input

--- content/data.js
+++ content/data.js
@@ -1,4 +1,23 @@
-// Get the values of the input
-let getInputValues = () => {
-  
+// Data
+exports.data = {
+  minTransmTempData: 0,
+  maxTransmTempData: 0,
+  minDesirTempData: 0,
+  maxDesirTempData: 0,
+  setPointData: 0,
+  sensitivityData: 0,
+  minFinContrPressData: 0,
+  maxFinContrPressData: 0
 }
+
+
+
+
+// Recalculates value on change
+exports.recalculate = () => {
+
+}
+
+
+
+// export {data, recalculate};
